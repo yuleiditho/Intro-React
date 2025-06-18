bab0d494288778f93e59b2a412320cb252758295
Denisse <yuleiditho@gmail.com>
2025-06-17 18:08:57 -0600

    feat: create App component to integrate Header and ShoppingList

---
+++ 02-shopping-list/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import ShoppingList from './components/ShoppingList'
+
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <ShoppingList/>
+    </div>
+  )
+}
+
+export default App
